2b407960669c2e9c0cdb10c3e9cdc4dbc2a2088f
Eshaan <eshaanpandeyk540@gmail.com>
2023-05-31 19:03:18 +0530

    eight commits

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,33 @@
-const express = require('express');
-const app = express();
-const mongoDB = require("./db");
-const dotenv = require('dotenv');
-const cors = require('cors');
-
-dotenv.config();
-
-mongoDB();
-
-app.use(express.json());
+const express = require('express')
+const app = express()
+const mongoDB = require("./db")
+const dotenv=require("dotenv")
+const cors=require("cors");
 app.use(cors());
 
-app.use('/api', require("./routes/CreateUser"));
-app.use('/api', require("./routes/DisplayData"));
-app.use('/api', require("./routes/OrderData"));
+dotenv.config()
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://foodtastic.vercel.app");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
+mongoDB();
 app.get('/', (req, res) => {
-  res.send('hello world');
-});
+  res.send('hello world')
+})
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// })
+app.use(express.json())
+app.use('/api', require("./routes/CreateUser"))
+app.use('/api', require("./routes/DisplayData"))
+app.use('/api', require("./routes/OrderData"))
+
+
+
 
 app.listen(5000, () => {
-  console.log("App is started on port 5000");
-});
+  console.log("App is started on port 5000")
+})
